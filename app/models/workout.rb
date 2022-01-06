b453fba219f00b080d42ece634192e71a783855b
Regina Jackson <rjackson156@gmail.com>
2022-01-05 22:11:22 -0500

    Calculate and store day of week when creating workout

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,2 +1,8 @@
 class Workout < ApplicationRecord
+    before_create :store_day_of_week
+
+    def store_day_of_week
+        day_of_week = self.date.strftime("%A")
+        self.day_of_week = day_of_week
+    end
 end
