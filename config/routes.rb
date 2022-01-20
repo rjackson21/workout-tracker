edf0da1409d8d8ef63bb5a7150b58c040532b7a3
Regina Jackson <rjackson156@gmail.com>
2022-01-20 18:17:21 -0500

    Set up about action in pages controller and about view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'workouts/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root to: 'pages#home'
+  get "/pages/about" , to: "pages#about"
 
   resources :workouts
   resources :category
