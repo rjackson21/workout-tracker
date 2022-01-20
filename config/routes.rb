238fc883236afce63190ab6466b60552a22598ce
Regina Jackson <rjackson156@gmail.com>
2022-01-20 17:44:54 -0500

    Set up pages controller with home action and home view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "workouts#index"
+  root to: 'pages#home'
 
   resources :workouts
   resources :category
