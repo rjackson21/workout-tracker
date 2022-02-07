b94f5f22f25bb1207a01e1abd1ec1ddc6abdd797
Regina Jackson <rjackson156@gmail.com>
2022-02-07 18:53:45 -0500

    Associate user and workout tables

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,6 +1,7 @@
 class Workout < ApplicationRecord
     before_create :store_day_of_week
     belongs_to :category
+    belongs_to :user
 
     def store_day_of_week
         day_of_week = self.date.strftime("%A")
