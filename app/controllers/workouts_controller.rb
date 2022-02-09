cb36484ccf7b9f7f8b0dcd7889884c71153b91ae
Regina Jackson <rjackson156@gmail.com>
2022-02-08 20:01:10 -0500

    Update index to only show workouts for current user

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,11 +1,14 @@
 class WorkoutsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destory]
+  
   def index
     @workouts = Workout.all
   end
 
   def create
     workout = Workout.new(workout_params)
-    
+
     if workout.save
       redirect_back fallback_location: root_path
     else
@@ -32,14 +35,21 @@ class WorkoutsController < ApplicationController
 
   def new
     @workout = Workout.new
+    
   end
 
   def destroy
     @workout = Workout.find(params[:id])
     @workout.destroy
-    
+    flash[:notice] = "You've deleted #{@workout.workout_type}"
     redirect_to root_path
   end  
+
+  def correct_user
+    @workout = current_user.workouts.find_by(id: params[:id])
+    redirect_to workouts_path, notice: "Not Authorized" if @workout.nil?
+  end
+
 
   private
 
